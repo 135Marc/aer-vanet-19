d632b4b9747918ac31f913d8c1c6375a88747a4f
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-19 10:40:49 +0100

    chack

--- adhoc_app.py
+++ adhoc_app.py
@@ -12,26 +12,8 @@ NAME = ''
 
 table = {
 }
-class PDU:
-    pdu_type = "HELLO"
-    ttl = 1
-    
-    def __init__(self, givenType, givenTTL):
-        self.pdu_type = givenType 
-        self.ttl = givenTTL
 
-    def getType(self):
-        return self.pdu_type
-
-    def getTTL(self):
-        return self.ttl
-
-    def setType(self, givenType):
-        self.pdu_type = givenType
-
-    def setTTL(self, givenTTL):
-        self.ttl = givenTTL
-
+from adhoc_pdu import PDU
 import time
 import struct
 import socket
