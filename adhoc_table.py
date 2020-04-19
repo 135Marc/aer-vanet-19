d291f527fa391ef5fd95f796d9e570a4be27342b
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-19 11:51:48 +0100

    chack

--- adhoc_table.py
+++ adhoc_table.py
@@ -13,6 +13,6 @@ class Table:
     def printTable(self):
         print('-------------------------------------')
         print(self.fields[0] + '|' + self.fields[1] + '|' + self.fields[2])
-        for i in range(0, len(nomes)):
+        for i in range(0, len(self.nomes)):
             print(self.nomes[i] + '|' + self.vizinhos[i] + '|' + self.enderecos[i])
         print('-------------------------------------')
