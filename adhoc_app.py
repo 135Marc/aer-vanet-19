41b6884ddd401156cb6857f70e52adba151fb196
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-22 17:58:00 +0100

    chack

--- adhoc_app.py
+++ adhoc_app.py
@@ -4,7 +4,11 @@
 # Requires that your OS kernel supports IP multicast.
 
 #Nome do nodo:
-#> python3 adhoc_app.py NOME
+#> python3 adhoc_app.py -n NOME 
+
+#Intervalo entre pedidos HELLO, em segundos:
+#> python3 adhoc_app.py -i INTERVALO
+
 
 import sys
 import threading
@@ -19,15 +23,25 @@ MYPORT = 9999
 MYGROUP_6 = 'ff02::1'
 NAME = ''
 ROUTING = Table()
+INTERVAL = 10
 
 def main():
     if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv))
+            switch(sys.argv[i])
+                case '-n':
+                        NAME = sys.argv[i+1]
+                        break
+                case '-i':
+                        INTERVAL = int(sys.argv[i+1])
+                        break
         NAME = sys.argv[1]
     else:
-        NAME = str(random.uniform(0, 100))
+        print('-n: nome')
+        print('-i: intervalo')
     print('Nodo: ' + NAME)
     ROUTING.addNode(NAME, NAME, MYGROUP_6)
-    y = threading.Thread(target=receiver, args=(socket, NAME, MYPORT, MYGROUP_6, ROUTING,))
+    y = threading.Thread(target=receiver, args=(socket, NAME, MYPORT, MYGROUP_6, ROUTING, INTERVAL,))
     y.start()
     x = threading.Thread(target=sender, args=(socket, NAME, MYPORT, MYGROUP_6, ROUTING,))
     x.start()
--- adhoc_sender.py
+++ adhoc_sender.py
@@ -2,11 +2,11 @@ import pickle
 import time
 from adhoc_pdu import PDU
 
-def sender(socket, name, port, groupipv6, routing_table):
+def sender(socket, name, port, groupipv6, routing_table, interval):
     sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, 0)
 
     while True:
         pdu = PDU(name, "HELLO" + name, 1, routing_table)
         sock.sendto(pickle.dumps(pdu), (groupipv6, port))
-        time.sleep(15)
+        time.sleep(interval)
