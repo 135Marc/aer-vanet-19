59e06ddf91a69033babf3431b03823b8e9913fa0
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-23 16:43:26 +0100

    chack

--- adhoc_table.py
+++ adhoc_table.py
@@ -10,22 +10,26 @@ class Table:
         self.neighbours = givenNeighbours.copy()
 
     def addNode(self, nome, vizinho, endereco, time):
+        #Verificar se o nodo já está na tabela (caso esteja na tabela será removido)
         rmrow = None
         for row in self.rows:
             if (row[0] == nome) & (nome == vizinho):
                 rmrow = (row[0], row[1], row[2], row[3])
         if rmrow:
             self.rows.remove(rmrow)
+        #Adicionar o nodo há tabela
         newrow = (nome, vizinho, endereco, time)
         self.rows.add(newrow)
 
     def addNeighbour(self, nome, vizinho, endereco, time):
+        #Verificar se o vizinho já existe (caso exista será removido)
         rmrow = None
         for row in self.neighbours:
             if (row[0] == nome) & (nome == vizinho):
                 rmrow = (row[0], row[1], row[2], row[3])
         if rmrow:
             self.neighbours.remove(rmrow)
+        #Adicionar o novo vizinho
         row = (nome, vizinho, endereco, time)
         self.neighbours.add(row)
 
@@ -36,18 +40,25 @@ class Table:
         return self.neighbours
 
     def mergeTable(self, table, source, time):
+        #Descobrir nodos de nivel dois que ainda não existem
         actual_nodes = set()
         incoming_nodes = set()
         for node in self.rows:
             actual_nodes.add(node[0])
         for node in table.getNeighbours():
-            incoming_nodes.add(node[0])
-
+            incoming_nodes.add(node[0]) 
         toadd_nodes = incoming_nodes - actual_nodes
+
+        #Adicionar nodos de nivel dois que ainda não existem
         for node in toadd_nodes:
             for row in table.getRows():
                 if(row[0] == node): 
                     self.rows.add((row[0], source, row[2], time))
+
+        #Atualizar o timestamp dos vizinhos deste nodo            
+        for row in self.rows:
+            if row[1] == source:
+                row[3] == time
 
     def verifyTimes(self, interval):
         actual_time = time.time()
