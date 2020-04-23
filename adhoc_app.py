cd7c9c8300bbab2867285fa14817f7b81c22535b
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-23 11:52:14 +0100

    chack

--- adhoc_app.py
+++ adhoc_app.py
@@ -38,7 +38,6 @@ def main():
         print('-i: intervalo')
     print('Nodo: ' + NAME)
     ROUTING.addNode(NAME, NAME, MYGROUP_6)
-    ROUTING.addNeighbour(NAME, NAME, MYGROUP_6)
     y = threading.Thread(target=receiver, args=(socket, NAME, MYPORT, MYGROUP_6, ROUTING,))
     y.start()
     x = threading.Thread(target=sender, args=(socket, NAME, MYPORT, MYGROUP_6, ROUTING, INTERVAL,))
