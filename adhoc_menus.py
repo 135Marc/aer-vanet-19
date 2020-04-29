dffedc7e89b988b3be89f643a8438afa33f9605c
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-29 21:56:22 +0100

    chack

--- adhoc_menus.py
+++ adhoc_menus.py
@@ -2,10 +2,20 @@ import queue
 from adhoc_pdu import PDU
 from adhoc_table import Table
 
-def menus(source, msgqueue):
+def menus(source, msgqueue, table):
     while True:
-        print('---------Nodo a encontrar:-----------')
-        nodo = input()
-        print('-------------------------------------')
-        newpdu = PDU(source, 'ROUTE_REQUES', 5, Table())
-        msgqueue.put(newpdu)
+        print('---------Opções:-----------')
+        print('Encontrar nodo: f')
+        print('Imprimir tabela: p')
+        print('---------------------------')
+        opt = input()
+        if(opt = 'p')
+            table.printTable()
+        elif(opt = 'f')
+            print('Encontrar nodo:')
+            nodo = input()
+            newpdu = PDU(source, 'ROUTE_REQUES', 5, Table())
+            msgqueue.put(newpdu)
+        else
+            print('Opção inválida.')
+
