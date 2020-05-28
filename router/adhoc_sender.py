0f51e28f4cc5f39822538e22418ac44cda9b74e4
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-28 01:47:23 +0100

    chack

--- router/adhoc_sender.py
+++ router/adhoc_sender.py
@@ -24,7 +24,7 @@ def dispatch(sock, msgqueue, groupipv6, port, rplyawait, interval, name, answers
     while True:
         # Obter proxima mensagem
         pdu = msgqueue.get()
-        
+        print('sender ' + pdu.getMsg())
         # Adicionar elemento há lista de espera por respostas 
         if(pdu.getType() == 'ROUTE_REQUEST'):
             rplyawait.addElem(pdu.getTarget())
