137755d9d6edb760626f8881f85a9bca5f0e48c1
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-30 20:15:07 +0100

    chack

--- fase1/adhoc_router.py
+++ fase1/adhoc_router.py
@@ -52,9 +52,9 @@ class Router:
                 self.pendingTable.add((directive,'ROUTE_REPLY'), source)
                 newpdu = PDU('ROUTE_REQUEST', source, None, ttl-1, None, directive, [self.name])
 
-                #Criar tread para remover elemento do array ao fim de um periodo de tempo
-                pt = threading.Thread(target=pendingTimeout, args=(self.timeout, self.pendingTable, (directive,'ROUTE_REPLY'),))
-                pt.start()
+                #Criar thread para remover elemento da pendingTable depois do passar o tempo de timeout
+                #pt = threading.Thread(target=pendingTimeout, args=(self.timeout, self.pendingTable, (directive,'ROUTE_REPLY'),))
+                #pt.start()
 
                 print('[ROUTE_REQUEST] forward')
 
