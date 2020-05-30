492356ab2f49c632c31de469b6c6f0236aaba064
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-30 12:35:09 +0100

    chack

--- fase1/adhoc_menus.py
+++ fase1/adhoc_menus.py
@@ -2,7 +2,6 @@ import queue
 from adhoc_pdu import PDU
 
 def menus(name, table):
-    print('Nó: ' + name)
     print('---------Opções:-----------')
     print('Encontrar nodo: find')
     print('Imprimir tabela: print')
@@ -13,11 +12,11 @@ def menus(name, table):
         cmd = opt.split(' ')
 
         # Imprimir tabela
-        if opt[0] == 'print':
+        if opt[0] == 'prt':
             table.printTable()
         
         # Encontrar novo nodo
-        # elif opt[0] == 'find':
+        # elif opt[0] == 'fnd':
             # print('Nome do nodo:')
             # nodo = input()
             # newpdu = PDU(name, 'ROUTE_REQUEST', 5, None, nodo, '', [source])
