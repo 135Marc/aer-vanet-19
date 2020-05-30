4b5233b9fe6279a724c0d3f5c8231255c6f4eb38
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-30 19:34:52 +0100

    chack

--- fase1/adhoc_router.py
+++ fase1/adhoc_router.py
@@ -21,7 +21,6 @@ class Router:
         newpdu = None
 
         # Obter tipo do pdu, ttl e target
-        pdu.printPDU()
         pdu_type = pdu.getType()
         ttl = pdu.getTTL()
         source = pdu.getSource()
