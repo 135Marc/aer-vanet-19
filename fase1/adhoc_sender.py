71549023bdd27705f5a28e0cdb9a4d1f049a501b
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-30 01:26:12 +0100

    chack

--- fase1/adhoc_sender.py
+++ fase1/adhoc_sender.py
@@ -4,15 +4,15 @@ import threading
 from adhoc_pdu import PDU
 from adhoc_table import Table
 
-def sender(socket, port, groupipv6, name, routing_table, zone, hello_interval, dead_interval):
+def sender(socket, port, groupipv6, name, routing_table, zone, hello_interval):
     sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, 0)
 
     # Enviar pdu's HELLO a cada hello_interval
-    ht = threading.Thread(target=hello, args=(sock, groupipv6, port, zone, name, routing_table, hello_interval, dead_interval,))
+    ht = threading.Thread(target=hello, args=(sock, groupipv6, port, zone, name, routing_table, hello_interval,))
     ht.start()
 
-def hello(sock, groupipv6, port, zone, name, routing_table, hello_interval, dead_interval):
+def hello(sock, groupipv6, port, zone, name, routing_table, hello_interval):
     while True:
         pdu = PDU('HELLO', name, None, 1, routing_table, zone, [name])
         sock.sendto(pickle.dumps(pdu), (groupipv6, port))
