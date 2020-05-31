c86366ea78f0a75125bbeed5690f962fa0ee2dfc
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-31 09:46:12 +0100

    chack

--- fase1/adhoc_menus.py
+++ fase1/adhoc_menus.py
@@ -22,7 +22,7 @@ def menus(name, router, radius, dispatch_queue):
                     router.pendingTable.add((cmd[1],'ROUTE_REPLY'), source)
                     print('[ROUTE_REQUEST] already requested')
                 else:
-                    router.pendingTable.add((cmd[1],'ROUTE_REPLY'), source)
+                    router.pendingTable.add((cmd[1],'ROUTE_REPLY'), name)
                     pdu = PDU('ROUTE_REQUEST', name, None, radius, None, cmd[1], [name])
                     dispatch_queue.put(pdu)
             else:
