4d481a2a3244ff18a7c88cd2882051de21328c86
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-05-31 13:01:33 +0100

    chack

--- fase3/adhoc_pending_timer.py
+++ fase3/adhoc_pending_timer.py
@@ -3,7 +3,7 @@ import sys
 
 def pendingTimeout(timeout, pendingTable, elem):
     time.sleep(timeout)
-    if pendingTable.check(elem):
-        pendingTable.rm(elem)
+    if pendingTable.check(elem[0]):
+        pendingTable.rm(elem[0])
         print('[TIMEOUT]', elem[1], elem[0])
         sys.exit()
